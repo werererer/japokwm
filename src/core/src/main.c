f73ee20e54fdc97c446d5a72498526f8a68cc337
werererer <jakobschlanstedt@gmail.com>
2020-10-24 12:26:09 +0200

    remove some tabs

--- src/core/src/main.c
+++ src/core/src/main.c
@@ -406,19 +406,19 @@ void createnotify(struct wl_listener *listener, void *data)
 
 void createnotifyLayerShell(struct wl_listener *listener, void *data)
 {
-	/* This event is raised when wlr_xdg_shell receives a new xdg surface from a
-	 * client, either a toplevel (application window) or popup. */
-	struct wlr_layer_surface_v1 *layer_surface = data;
-	struct client *c;
+    /* This event is raised when wlr_xdg_shell receives a new xdg surface from a
+     * client, either a toplevel (application window) or popup. */
+    struct wlr_layer_surface_v1 *layer_surface = data;
+    struct client *c;
 
 
-	/* Allocate a Client for this surface */
-	c = layer_surface->data = malloc(sizeof(*c));
-	c->surface.layer = layer_surface;
-	c->bw = borderPx;
-	c->type = LayerShell;
+    /* Allocate a Client for this surface */
+    c = layer_surface->data = malloc(sizeof(*c));
+    c->surface.layer = layer_surface;
+    c->bw = borderPx;
+    c->type = LayerShell;
 
-	/* Listen to the various events it can emit */
+    /* Listen to the various events it can emit */
     c->commit.notify = commitnotify;
     wl_signal_add(&layer_surface->surface->events.commit, &c->commit);
     c->map.notify = maprequest;
