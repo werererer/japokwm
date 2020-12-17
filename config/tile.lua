f97ce5fb14a7a630c8baea2001aefcca536c24db
Jakob Schlanstedt <johann123j@gmail.com>
2020-12-17 19:45:25 +0100

    further improve resizing

--- config/tile.lua
+++ config/tile.lua
@@ -166,15 +166,18 @@ function count(i, j, d)
             elseif d == Direction.BOTTOM then
                 resize = con[2] >= container[2]
             elseif d == Direction.LEFT then
-                resize = container[1] > con[1]
+                resize = con[1] < container[1]
             elseif d == Direction.RIGHT then
-                resize = container[1] < con[1]
+                resize = con[1] > container[1]
+                over = con[2] >= container[2] + container[4]
+                under = con[2] + con[4] <= container[2]
+                resize = resize and not (over or under)
+                print(resize, con[2] > container[2] + container[4], con[2] + con[4] < container[2])
             end
 
             if resize then
                 startv = container[1] + container[3]
                 local d = {con[1]-startv, con[2], con[3], con[4], i, j2}
-                print("x/x2:", con[1], startv, full)
                 d[1] = d[1]/full
                 d[2] = d[2]
                 d[3] = d[3]/full
@@ -221,13 +224,43 @@ function resizeAll(i, j, n, d)
         local lj = g[k][6]
         local startv = layoutData[i][j][1] + layoutData[i][j][3]
         local newWidth = 1 - startv
-        print("start", li, lj, startv, newWidth)
         layoutData[li][lj][1] = startv + (g[k][1] * newWidth)
-        print("continue")
         layoutData[li][lj][2] = g[k][2]
         layoutData[li][lj][3] = g[k][3] * newWidth
         layoutData[li][lj][4] = g[k][4]
     end
+    -- layoutData[i][j2] = moveResize(con, nmove, -n/k, d)
+
+    -- for j2 = startv,endv,step do
+    --     local con = layoutData[i][j2]
+    --     local resize = false
+    --     local nresize = false
+
+    --     if j2 == j then
+    --     else
+    --         -- resize container[i][j]?
+    --         if d == Direction.TOP then
+    --             resize = con[2] <= container[2]
+    --         elseif d == Direction.BOTTOM then
+    --             resize = con[2] >= container[2]
+    --         elseif d == Direction.LEFT then
+    --             resize = container[1] > con[1]
+    --         elseif d == Direction.RIGHT then
+    --             resize = container[1] < con[1]
+    --         end
+
+    --         if resize then
+    --             -- print("if resize")
+    --             -- -- k is the number of windows right to this (this
+    --             -- -- window included)
+    --             -- -- layoutData[i][j2] = moveResize(con, nmove, -n/k, d)
+    --             -- layoutData[i][j2] = moveResize(con, nmove, -n/k, d)
+    --             -- -- nmove is the move of the next window right to
+    --             -- -- this
+    --             -- nmove = getNextMove(nmove, -n/k)
+    --         end
+    --     end
+    -- end
 end
 
 function resizeMainAll(n, d)
