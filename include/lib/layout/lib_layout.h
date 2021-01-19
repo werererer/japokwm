c64c9837cbb67dc4b820fe99eb42d34fc44387a5
Jakob Schlanstedt <johann123j@gmail.com>
2021-01-19 22:28:11 +0100

    add missing files

---
+++ include/lib/layout/lib_layout.h
@@ -0,0 +1,9 @@
+#ifndef LIB_LAYOUT_H
+#define LIB_LAYOUT_H
+
+#include <lua.h>
+#include <lauxlib.h>
+
+int set_layout(lua_State *L);
+
+#endif /* LIB_LAYOUT_H */
---
+++ src/lib/layout/lib_layout.c
@@ -0,0 +1,26 @@
+#include "lib/layout/lib_layout.h"
+
+#include "monitor.h"
+#include "server.h"
+
+int set_layout(lua_State *L)
+{
+    struct monitor *m = selected_monitor;
+    struct workspace *ws = m->ws;
+    struct layout *lt = &ws->layout;
+
+    // reset options
+    copy_options(&lt->options, &server.options);
+
+    // 3. argument
+    lt->lua_box_data_index = lua_copy_table(L);
+    // 2. argument
+    lt->lua_layout_master_copy_data_index = lua_copy_table(L);
+    // 1.argument
+    lt->lua_layout_copy_data_index = lua_copy_table(L);
+
+    lua_rawgeti(L, LUA_REGISTRYINDEX, lt->lua_layout_copy_data_index);
+    lt->lua_layout_original_copy_data_index = lua_copy_table(L);
+    return 0;
+}
+
