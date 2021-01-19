6096d21ff0fa705b8489dacfa0114a9e24d513a7
Jakob Schlanstedt <johann123j@gmail.com>
2021-01-19 17:20:16 +0100

    cleanup

---
+++ config/layouts/monocle/init.lua
@@ -0,0 +1,29 @@
+function Update(n)
+    print("update: ", n)
+end
+
+action.set_resize_direction(0)
+local layout_data = {
+    {
+        {0, 0, 1, 1},
+    },
+}
+
+local master_layout_data = {
+    {
+        {0.0, 0.0, 1.0, 1.0}
+    },
+    {
+        {0.0, 0.0, 1.0, 0.5},
+        {0.0, 0.5, 1.0, 0.5},
+    },
+}
+
+local box_data = {
+    {1},
+}
+
+action.set_layout(layout_data, master_layout_data, box_data)
+action.set_arrange_by_focus(true);
+lconfig.set_borderpx(0)
+lconfig.set_gaps(0, 0)
---
+++ config/layouts/two_pane/init.lua
@@ -0,0 +1,30 @@
+function Update(n)
+end
+action.set_resize_direction(Direction.LEFT)
+
+local layout_data = {
+    {
+        {0, 0, 1, 1},
+    },
+    {
+        {0.5, 0.0, 0.5, 1.0},
+        {0.0, 0.0, 0.5, 1.0},
+    },
+}
+
+local master_layout_data = {
+    {
+        {0.0, 0.0, 1.0, 1.0}
+    },
+    {
+        {0.0, 0.0, 1.0, 0.5},
+        {0.0, 0.5, 1.0, 0.5},
+    },
+}
+
+local box_data = {
+    {1},
+    {2},
+}
+
+action.set_layout(layout_data, master_layout_data, box_data)
