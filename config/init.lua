f33d61384d2770cc25d43dad7991092e56279640
Jakob Schlanstedt <johann123j@gmail.com>
2021-01-23 01:29:15 +0100

    remove default config file

--- config/init.lua
+++ config/init.lua
@@ -1,4 +1,15 @@
-require "defaultConfig"
+require "tile"
+
+Cursor_mode = {
+    CUR_NORMAL = 0,
+    CUR_MOVE = 1,
+    CUR_RESIZE = 2,
+}
+
+-- TODO complete transformations
+Monitor_transformation = {
+    NORMAL = 0,
+}
 
 config.set_sloppy_focus(true)
 config.set_borderpx(2)
