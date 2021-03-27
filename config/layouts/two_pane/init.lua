cdf5b1a2c241722efa18164d09cccd05c86243a6
Jakob Schlanstedt <johann123j@gmail.com>
2021-03-27 19:37:24 +0100

    update layouts

--- config/layouts/two_pane/init.lua
+++ config/layouts/two_pane/init.lua
@@ -3,14 +3,23 @@ local layout_data = {
         {0, 0, 1, 1},
     },
     {
-        {0.5, 0.0, 0.5, 1.0},
         {0.0, 0.0, 0.5, 1.0},
+        {0.5, 0.0, 0.5, 1.0},
     },
 }
 
+local function update(n)
+    if n <= 1 then
+        l.config.set_tile_borderpx(0)
+    else
+        l.config.set_tile_borderpx(2)
+    end
+end
+
 layout.set(layout_data)
+l.event.set_update_function(update)
 l.config.set_inner_gaps(0)
 l.config.set_outer_gaps(0)
-l.config.set_resize_direction(info.direction.left)
+l.config.set_resize_direction(info.direction.right)
 l.config.set_layout_constraints({min_width = 0.1, max_width = 1, min_height = 0.1, max_height = 1})
 l.config.set_master_constraints({min_width = 0.2, max_width = 1, min_height = 0.2, max_height = 1})
