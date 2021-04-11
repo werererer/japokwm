aa6a7aaa9749384359e28f36deb71f635bfde309
Jakob Schlanstedt <johann123j@gmail.com>
2021-04-11 02:49:19 +0200

    (refactor): refactor init_server function

--- src/main.c
+++ src/main.c
@@ -144,46 +144,7 @@ static void run(char *startup_cmd)
 
 static int setup()
 {
-    wl_list_init(&sticky_stack);
-
-    wlr_list_init(&server.mons);
-    wlr_list_init(&server.popups);
-    wlr_list_init(&server.visual_stack_lists);
-    wlr_list_init(&server.normal_visual_stack_lists);
-    wlr_list_init(&server.layer_visual_stack_lists);
-
-    wlr_list_init(&server.tiled_visual_stack);
-    wlr_list_init(&server.floating_visual_stack);
-    wlr_list_init(&server.layer_visual_stack_background);
-    wlr_list_init(&server.layer_visual_stack_bottom);
-    wlr_list_init(&server.layer_visual_stack_top);
-    wlr_list_init(&server.layer_visual_stack_overlay);
-
-    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_overlay);
-    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_top);
-    wlr_list_push(&server.visual_stack_lists, &server.floating_visual_stack);
-    wlr_list_push(&server.visual_stack_lists, &server.tiled_visual_stack);
-    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_bottom);
-    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_background);
-
-    wlr_list_push(&server.normal_visual_stack_lists, &server.floating_visual_stack);
-    wlr_list_push(&server.normal_visual_stack_lists, &server.tiled_visual_stack);
-
-    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_overlay);
-    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_top);
-    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_bottom);
-    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_background);
-
-    wlr_list_init(&server.scratchpad);
-    wlr_list_init(&server.workspaces);
-
-    wlr_list_init(&server.client_lists);
-
-    wlr_list_init(&server.normal_clients);
-    wlr_list_init(&server.independent_clients);
-
-    wlr_list_push(&server.client_lists, &server.normal_clients);
-    wlr_list_push(&server.client_lists, &server.independent_clients);
+    init_server();
 
     L = luaL_newstate();
     luaL_openlibs(L);
@@ -363,8 +324,6 @@ void print_usage()
 int main(int argc, char *argv[])
 {
     char *startup_cmd = "";
-
-    init_server();
 
     static struct option long_options[] = {
         {"help", no_argument, NULL, 'h'},
--- src/server.c
+++ src/server.c
@@ -10,4 +10,45 @@ void init_server()
         .config_dir = "",
         .previous_workspace_id = INVALID_POSITION,
     };
+
+    wl_list_init(&sticky_stack);
+
+    wlr_list_init(&server.mons);
+    wlr_list_init(&server.popups);
+    wlr_list_init(&server.visual_stack_lists);
+    wlr_list_init(&server.normal_visual_stack_lists);
+    wlr_list_init(&server.layer_visual_stack_lists);
+
+    wlr_list_init(&server.tiled_visual_stack);
+    wlr_list_init(&server.floating_visual_stack);
+    wlr_list_init(&server.layer_visual_stack_background);
+    wlr_list_init(&server.layer_visual_stack_bottom);
+    wlr_list_init(&server.layer_visual_stack_top);
+    wlr_list_init(&server.layer_visual_stack_overlay);
+
+    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_overlay);
+    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_top);
+    wlr_list_push(&server.visual_stack_lists, &server.floating_visual_stack);
+    wlr_list_push(&server.visual_stack_lists, &server.tiled_visual_stack);
+    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_bottom);
+    wlr_list_push(&server.visual_stack_lists, &server.layer_visual_stack_background);
+
+    wlr_list_push(&server.normal_visual_stack_lists, &server.floating_visual_stack);
+    wlr_list_push(&server.normal_visual_stack_lists, &server.tiled_visual_stack);
+
+    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_overlay);
+    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_top);
+    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_bottom);
+    wlr_list_push(&server.layer_visual_stack_lists, &server.layer_visual_stack_background);
+
+    wlr_list_init(&server.scratchpad);
+    wlr_list_init(&server.workspaces);
+
+    wlr_list_init(&server.client_lists);
+
+    wlr_list_init(&server.normal_clients);
+    wlr_list_init(&server.independent_clients);
+
+    wlr_list_push(&server.client_lists, &server.normal_clients);
+    wlr_list_push(&server.client_lists, &server.independent_clients);
 }
