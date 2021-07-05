8a2b1b88f97f1f53ee72b03256d973829aa4570f
Jakob Schlanstedt <johann123j@gmail.com>
2021-07-05 15:01:40 +0200

    fix: fix bug where tags aren't always selected

--- src/monitor.c
+++ src/monitor.c
@@ -228,6 +228,7 @@ void monitor_focus_tags(int ws_id, struct BitSet bitset)
     if (!tagset) {
         tagset = create_tagset(selected_monitor, ws_id, bitset);
     }
+    bitset_move(&tagset->workspaces, &bitset);
 
     push_tagset(tagset);
 }
