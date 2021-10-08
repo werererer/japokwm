bd5ac6df4a6b02749024dc21271764000a32a5f6
Jakob Schlanstedt <johann123j@gmail.com>
2021-10-08 02:05:29 +0200

    feat: make containers object oriented in lua

--- include/lib/actions/lib_container.h
+++ include/lib/actions/lib_container.h
@@ -6,13 +6,21 @@
 #include <lualib.h>
 #include <stdbool.h>
 
+
 // TODO fix later
+// methods
+int lib_container_get_focused(lua_State *L);
 int lib_container_get_workspace(lua_State *L);
-int lib_container_set_alpha(lua_State *L);
 int lib_container_set_ratio(lua_State *L);
 int lib_container_set_sticky(lua_State *L);
 int lib_container_set_sticky_restricted(lua_State *L);
 int lib_container_toggle_add_sticky(lua_State *L);
 int lib_container_toggle_add_sticky_restricted(lua_State *L);
+
+// getter
+int lib_container_get_alpha(lua_State *L);
+
+// setter
+int lib_container_set_alpha(lua_State *L);
 
 #endif /* LIB_CONTAINER_H */
