eb3d783698913b36c3f4a5b0a8309eb47f47d31a
Jakob Schlanstedt <johann123j@gmail.com>
2021-10-31 15:39:11 +0100

    feat: add Layout.toggle

--- config/init.lua
+++ config/init.lua
@@ -1,2 +1,4 @@
 local layouts = {"two_pane", "three_columns"}
 server.default_layout_ring = Ring.new(layouts)
+
+opt:bind_key("mod-S-b", function() Layout.toggle("monocle") end)
--- src/utils/parseConfigUtils.c
+++ src/utils/parseConfigUtils.c
@@ -297,7 +297,7 @@ static void *_notify_msg(void *arg)
     }
 
     notify_init(msg);
-    NotifyNotification* n = notify_notification_new("Error in config file", 
+    NotifyNotification* n = notify_notification_new("Japokwm.message: ", 
             msg,
             0);
     notify_notification_set_timeout(n, 10000); // 10 seconds
