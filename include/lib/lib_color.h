4473a63d71b6e9d0f90e827ef51c7e8d1c895df3
Jakob Schlanstedt <johann123j@gmail.com>
2021-12-14 18:16:17 +0100

    feat: add extra colors

--- include/lib/lib_color.h
+++ include/lib/lib_color.h
@@ -10,4 +10,12 @@ int lib_color_new(lua_State *L);
 
 struct color check_color(lua_State *L, int narg);
 
+// static setters
+// static getters
+int lib_color_get_black(lua_State *L);
+int lib_color_get_white(lua_State *L);
+int lib_color_get_red(lua_State *L);
+int lib_color_get_green(lua_State *L);
+int lib_color_get_blue(lua_State *L);
+
 #endif /* LIB_COLOR_H */
