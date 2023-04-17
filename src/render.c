1c29593171116625dc1e4475b1f81ce01f2c23a4
Jakob Schlanstedt <johann123j@gmail.com>
2023-04-17 12:09:24 +0200

    feat: implement ground structure for scene_graph

--- src/render.c
+++ src/render.c
@@ -8,6 +8,8 @@
 #include "tag.h"
 #include "monitor.h"
 #include "layout.h"
+#include "list_sets/container_stack_set.h"
+#include "root.h"
 
 static void surface_handle_destroy(struct wl_listener *listener, void *data)
 {
@@ -21,6 +23,41 @@ static void surface_handle_destroy(struct wl_listener *listener, void *data)
     free(surface);
 }
 
+// TODO refactor the name it doesn't represent what this does perfectly
+// returns the newly accquired hidden edges
+static enum wlr_edges container_update_hidden_edges(struct container *con, struct wlr_box *borders, enum wlr_edges hidden_edges)
+{
+    struct monitor *m = container_get_monitor(con);
+
+    enum wlr_edges containers_hidden_edges = WLR_EDGE_NONE;
+    struct wlr_box con_geom = container_get_current_geom(con);
+    // int border_width = container_get_border_width(con);
+    // hide edges if needed
+    if (hidden_edges & WLR_EDGE_LEFT) {
+        if (con_geom.x == m->root->geom.x) {
+            containers_hidden_edges |= WLR_EDGE_LEFT;
+        }
+    }
+    if (hidden_edges & WLR_EDGE_RIGHT) {
+        if (is_approx_equal(con_geom.x + con_geom.width, m->root->geom.x + m->root->geom.width, 3)) {
+            containers_hidden_edges |= WLR_EDGE_RIGHT;
+        }
+    }
+    if (hidden_edges & WLR_EDGE_TOP) {
+        if (con_geom.y == m->root->geom.y) {
+            containers_hidden_edges |= WLR_EDGE_TOP;
+        }
+    }
+    if (hidden_edges & WLR_EDGE_BOTTOM) {
+        if (is_approx_equal(con_geom.y + con_geom.height, m->root->geom.y + m->root->geom.height, 3)) {
+            containers_hidden_edges |= WLR_EDGE_BOTTOM;
+        }
+    }
+
+    container_set_hidden_edges(con, containers_hidden_edges);
+    return containers_hidden_edges;
+}
+
 static void surface_handle_commit(struct wl_listener *listener, void *data) {
     struct scene_surface *surface = wl_container_of(listener, surface, commit);
 
@@ -32,9 +69,18 @@ static void surface_handle_commit(struct wl_listener *listener, void *data) {
             struct wlr_scene_rect *border = surface->borders[i];
             wlr_scene_node_set_enabled(&border->node, false);
         }
+        return;
     }
 
+    struct wlr_box *borders = (struct wlr_box[4]) {
+        container_get_current_border_geom(con, WLR_EDGE_TOP),
+        container_get_current_border_geom(con, WLR_EDGE_BOTTOM),
+        container_get_current_border_geom(con, WLR_EDGE_LEFT),
+        container_get_current_border_geom(con, WLR_EDGE_RIGHT),
+    };
+
     enum wlr_edges hidden_edges = WLR_EDGE_NONE;
+    struct monitor *m = container_get_monitor(con);
     struct tag *tag = monitor_get_active_tag(m);
     struct layout *lt = tag_get_layout(tag);
     if (lt->options->smart_hidden_edges) {
@@ -47,15 +93,6 @@ static void surface_handle_commit(struct wl_listener *listener, void *data) {
         lt->options->hidden_edges);
     }
 
-
-    struct wlr_box *borders = (struct wlr_box[4]) {
-        container_get_current_border_geom(con, WLR_EDGE_TOP),
-        container_get_current_border_geom(con, WLR_EDGE_BOTTOM),
-        container_get_current_border_geom(con, WLR_EDGE_LEFT),
-        container_get_current_border_geom(con, WLR_EDGE_RIGHT),
-    };
-
-    struct monitor *m = container_get_monitor(con);
     struct container *sel = monitor_get_focused_container(m);
     const struct color color = (con == sel) ? lt->options->focus_color :
     lt->options->border_color;
@@ -63,6 +100,13 @@ static void surface_handle_commit(struct wl_listener *listener, void *data) {
     for (int i = 0; i < BORDER_COUNT; i++) {
         struct wlr_scene_rect *border = surface->borders[i];
         struct wlr_box geom = borders[i];
+
+        bool is_hidden = hidden_edges & (1 << i);
+        // hide or show the border
+        wlr_scene_node_set_enabled(&border->node, !is_hidden);
+        if (is_hidden) {
+            continue;
+        }
 
         wlr_scene_node_set_position(&border->node, geom.x, geom.y);
         wlr_scene_rect_set_size(border, geom.width, geom.height);
@@ -72,6 +116,7 @@ static void surface_handle_commit(struct wl_listener *listener, void *data) {
         wlr_scene_rect_set_color(border, border_color);
     }
 }
+
 
 // static void render_borders(struct container *con, struct monitor *m,
 // pixman_region32_t *output_damage)
